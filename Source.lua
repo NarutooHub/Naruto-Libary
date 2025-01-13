363c0163c8227f68010c49e6ccf46a7d28cd3eb7
Laelmano24 <brockbomba@gmail.com>
2025-01-13 12:51:05 -0300

    update

--- Source.lua
+++ Source.lua
@@ -48,9 +48,9 @@ local redzlib = {
     },
     Orange = {
       ["Color Hub 1"] = ColorSequence.new({
-        ColorSequenceKeypoint.new(0.00, Color3.fromRGB(51, 36, 0)),
-        ColorSequenceKeypoint.new(0.50, Color3.fromRGB(51, 36, 0)),
-        ColorSequenceKeypoint.new(1.00, Color3.fromRGB(51, 36, 0))
+        ColorSequenceKeypoint.new(0.00, Color3.fromRGB(0, 0, 0)),
+        ColorSequenceKeypoint.new(0.50, Color3.fromRGB(0, 0, 0)),
+        ColorSequenceKeypoint.new(1.00, Color3.fromRGB(0, 0, 0))
       }),
       ["Color Hub 2"] = Color3.fromRGB(255, 145, 29),
       ["Color Stroke"] = Color3.fromRGB(255, 145, 29),

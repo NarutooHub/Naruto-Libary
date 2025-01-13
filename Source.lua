353430b13ddfb9b1275a9a42daa79aca8d34b14e
Laelmano24 <brockbomba@gmail.com>
2025-01-13 13:15:11 -0300

    update

--- Source.lua
+++ Source.lua
@@ -52,9 +52,9 @@ local redzlib = {
         ColorSequenceKeypoint.new(0.50, Color3.fromRGB(105, 68, 0)),
         ColorSequenceKeypoint.new(1.00, Color3.fromRGB(70, 40, 0))
       }),
-      ["Color Hub 2"] = Color3.fromRGB(232, 151, 0),
-      ["Color Stroke"] = Color3.fromRGB(0, 139, 78),
-      ["Color Theme"] = Color3.fromRGB(201, 130, 0),
+      ["Color Hub 2"] = Color3.fromRGB(188, 123, 0),
+      ["Color Stroke"] = Color3.fromRGB(200, 130, 0),
+      ["Color Theme"] = Color3.fromRGB(165, 106, 0),
       ["Color Text"] = Color3.fromRGB(243, 243, 243),
       ["Color Dark Text"] = Color3.fromRGB(180, 180, 180)
     }
